3433cf35b4b17b861045ac43721aa2dbf7d3aac3
Atharv Kasar <108543702+AtharvKasar04@users.noreply.github.com>
2025-09-05 18:46:46 +0530

    Update WorkTimeline.tsx

--- client/src/components/WorkTimeline.tsx
+++ client/src/components/WorkTimeline.tsx
@@ -20,7 +20,7 @@ interface WorkExperience {
 
 const workExperience: WorkExperience[] = [
   {
-    duration: "May 2025 - July - 2025",
+    duration: "May 2025 - Aug 2025",
     title: "Engineering Intern",
     company: "SoftDEL Systems Pvt. Ltd. (UNIDEL)",
     description: "Worked on EMS Dashboards and other Frontend aspects using various technologies (Angular, Vue JS), Implemented AWS Cloud Services in Deployment of projects. Built productible Web-Apps from scratch with Project Proposals."
